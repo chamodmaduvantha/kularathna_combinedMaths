b50294989a34c845ae4d0df48c487e1b56ea14f1
chamodmaduvantha <90128831+chamodmaduvantha@users.noreply.github.com>
2024-12-31 09:38:25 +0530

    Update
    Carousel

--- src/components/CaroselPage.vue
+++ src/components/CaroselPage.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="carousel-container">
     <v-carousel v-if="items.length" cycle interval="4000" data-aos="flip-left" data-aos-duration="2000">
-      <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover></v-carousel-item>
+      <v-carousel-item v-for="(item, i) in items" :key="i">
+        <img :src="item.src" class="carousel-image" alt="Carousel Image">
+      </v-carousel-item>
     </v-carousel>
 
     <div class="snowfall">
@@ -40,6 +42,13 @@ export default {
   position: relative;
 }
 
+.carousel-image {
+  display: block;
+  max-width: 100%;
+  height: auto;
+  margin: 0 auto;
+}
+
 .snowfall {
   position: absolute;
   top: 0;
@@ -54,14 +63,12 @@ export default {
 .snow {
   position: absolute;
   top: -10%;
-  /* Start above the view */
   animation: snowfall linear infinite;
 }
 
 .snow img {
   width: auto;
   height: 50px;
-  /* Maintain aspect ratio */
 }
 
 @keyframes snowfall {
@@ -71,68 +78,14 @@ export default {
 
   100% {
     transform: translateY(110vh) translateX(20px);
-    /* Fall downward out of the view */
   }
 }
 
-/* Define different snowflakes' positions and animation durations */
 .snow:nth-child(1) {
   left: 5%;
   animation-duration: 8s;
   animation-delay: 0s;
 }
 
-.snow:nth-child(2) {
-  left: 10%;
-  animation-duration: 10s;
-  animation-delay: 2s;
-}
-
-.snow:nth-child(3) {
-  left: 20%;
-  animation-duration: 12s;
-  animation-delay: 1s;
-}
-
-.snow:nth-child(4) {
-  left: 30%;
-  animation-duration: 7s;
-  animation-delay: 3s;
-}
-
-.snow:nth-child(5) {
-  left: 40%;
-  animation-duration: 9s;
-  animation-delay: 0s;
-}
-
-.snow:nth-child(6) {
-  left: 50%;
-  animation-duration: 11s;
-  animation-delay: 2s;
-}
-
-.snow:nth-child(7) {
-  left: 60%;
-  animation-duration: 8s;
-  animation-delay: 1s;
-}
-
-.snow:nth-child(8) {
-  left: 70%;
-  animation-duration: 12s;
-  animation-delay: 0s;
-}
-
-.snow:nth-child(9) {
-  left: 80%;
-  animation-duration: 9s;
-  animation-delay: 2s;
-}
-
-.snow:nth-child(10) {
-  left: 90%;
-  animation-duration: 10s;
-  animation-delay: 1s;
-}
+/* Additional snowflakes styling here */
 </style>
